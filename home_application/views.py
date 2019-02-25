b049037e2e249ab5ef8ee2f64fce1d04c8a95624
owenlin <owenlin@canway.net>
2019-02-25 19:20:31 +0800

    新建项目

---
+++ home_application/views.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import json
+
+import requests
+from django.http import JsonResponse
+
+from account.decorators import login_exempt
+from common.mymako import render_mako_context
+from common.mymako import render_json
+from conf.default import APP_ID, APP_TOKEN, BK_PAAS_HOST
+
+
+def home(request):
+    """
+    首页
+    """
+    return render_mako_context(request, '/home_application/home.html')
+
+
+def dev_guide(request):
+    """
+    开发指引
+    """
+    return render_mako_context(request, '/home_application/dev_guide.html')
+
+
+def contactus(request):
+    """
+    联系我们
+    """
+    return render_mako_context(request, '/home_application/contact.html')
+
+
+def test(request):
+    """
+    测试
+    """
+    return render_mako_context(request, '/home_application/test.html')
+
+
+def history(request):
+    return render_mako_context(request, '/home_application/history.html')
+
+
+def search_set(request):
