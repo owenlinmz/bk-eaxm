1ba1ac01b0afb8cc57322f2cc06f590afe23d228
owenlin <owenlin@canway.net>
2019-03-27 14:27:44 +0800

    修改配置

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -11,10 +11,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+        'NAME': 'exam_owenlin',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '10.0.1.16',                   		   # 数据库HOST
+        'PASSWORD': 'root',                        # 你的数据库password
+        'HOST': '192.168.200.18',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
